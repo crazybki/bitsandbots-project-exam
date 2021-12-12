2dc82d5834c89cf201b4f91b128122497156e578
Bjørge Kristiansen <41273107+crazybki@users.noreply.github.com>
2021-12-12 03:18:23 +0100

    image update

--- src/components/maingamepage/Gamepage/singlegame/imgslider/ImageSlider.js
+++ src/components/maingamepage/Gamepage/singlegame/imgslider/ImageSlider.js
@@ -5,10 +5,10 @@ function ImageSlider(props) {
     console.log(props)
     return (
         <>
-            <Carousel>
+            <Carousel interval={null}>
                 {props.images.screenshots.map(imgtag => {
                     console.log(imgtag.id)
-                    return <Carousel.Item>
+                    return <Carousel.Item className="img-fluid">
                         <img key={imgtag.id} alt={`the game ${imgtag.name}`} src={`https://images.igdb.com/igdb/image/upload/t_screenshot_big/${imgtag.image_id}.jpg`}
                             className="d-block mx-auto imgcontainer "
                         />
